597c65e6cbdf9b648a54d93b347c1eb218a754f5
yuneizemota-hub <yuneizemota@gmail.com>
2025-11-09 19:30:31 +0400

    Update ex-1 resolution .py

--- Exercise 01 - Coding is Cool/ex-1 resolution .py
+++ Exercise 01 - Coding is Cool/ex-1 resolution .py
@@ -1,4 +1,5 @@
-word1 = "Coding"
-word2 = "is"
-word3 = "cool"
+word1 = "Coding" # First word with a space at the end
+word2 = "is" # Second word with a space at the end
+word3 = "cool" # Third word with a space at the end
+# Combine (concatenate) all three words and print the result
 print(word1 + word2 + word3)
