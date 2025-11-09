9bad3d68f802513dc36c100ef078d1cc84b176d1
yuneizemota-hub <yuneizemota@gmail.com>
2025-11-09 19:44:12 +0400

    Update ex 5 resolution .py

--- Exercise 05 - Days of the Month/ex 5 resolution .py
+++ Exercise 05 - Days of the Month/ex 5 resolution .py
@@ -1,6 +1,6 @@
 # Dictionary in which month numbers are the keys and days are the values
 days_in_month ={
-1: 31, February: 28, 3: 31, 4: 30, 5: 31, 6: 30,
+1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 # month number from the user
 month = int(input("Enter the month number (1-12): "))
