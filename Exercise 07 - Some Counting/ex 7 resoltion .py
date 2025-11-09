a813ec173bba31e02695a75882a9347560c4bac2
yuneizemota-hub <yuneizemota@gmail.com>
2025-11-09 19:16:42 +0400

    Create ex 7 resoltion .py

---
+++ Exercise 07 - Some Counting/ex 7 resoltion .py
@@ -0,0 +1,20 @@
+#Count up from 0 to 50 in increments of 1
+print("Counting up from 0 to 50:")
+for number in range(0, 51):
+    print(number)
+#Count down from 50 to 0 in decrements of 1
+print("\nCounting down from 50 to 0:")
+for number in range(50, -1, -1):
+    print(number)
+#Count up from 30 to 50 in increments of 1
+print("\nCounting up from 30 to 50:")
+for number in range(30, 51):
+    print(number)
+#Count down from 50 to 10 in decrements of 2
+print("\nCounting down from 50 to 10 in decrements of 2:")
+for number in range(50, 9, -2):
+    print(number)
+#Count up from 100 to 200 in increments of 5
+print("\nCounting up from 100 to 200 in increments of 5:")
+for number in range(100, 201, 5):
+    print(number)
