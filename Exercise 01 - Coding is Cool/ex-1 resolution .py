9efaa141b055543842f3a671f82ead237296715d
yuneizemota-hub <yuneizemota@gmail.com>
2025-11-09 20:14:17 +0400

    Delete ex-1 resolution .py

--- Exercise 01 - Coding is Cool/ex-1 resolution .py
+++
@@ -1,5 +0,0 @@
-word1 = "Coding" # First word with a space at the end
-word2 = "is" # Second word with a space at the end
-word3 = "cool" # Third word with a space at the end
-# Combine (concatenate) all three words and print the result
-print(word1 + word2 + word3)
