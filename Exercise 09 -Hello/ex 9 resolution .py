0689452bc4cf5846bb0bd0bcb200b8d0b2da6326
yuneizemota-hub <yuneizemota@gmail.com>
2025-11-09 19:16:56 +0400

    Create ex 9 resolution .py

---
+++ Exercise 09 -Hello/ex 9 resolution .py
@@ -0,0 +1,6 @@
+def hello():
+    print("Hello")  # Fill in this blank to print "Hello" to the console
+def main():
+    hello()  # Fill in this blank to call the hello() function
+if __name__ == "__main__":
+    main()
