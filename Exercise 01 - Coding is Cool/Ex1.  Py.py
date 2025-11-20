2d937b33a126d5ceab7d374cde43ccfc8ccb56ba
yuneizemota-hub <yuneizemota@gmail.com>
2025-11-20 20:09:32 +0400

    .

--- Exercise 01 - Coding is Cool/Ex1.  Py.py
+++
@@ -1,5 +0,0 @@
-# Define three separate strings
-word1 = "Coding "
-word2 = "is "
-word3 = "cool "
-print(word1 + word2 + word3)
--- Exercise 01 - Coding is Cool/ex 1 .py
+++
@@ -1,6 +0,0 @@
-# Define three separate strings
-word1 = "Coding "
-word2 = "is "
-word3 = "cool "
-# print the result
-print(word1 + word2 + word3)
